ecd7dd8a044cbb045f6d84c482670b3a4119422b
malekawedan <malekaweden30@gmail.com>
2020-09-12 19:08:01 +0200

    aboute

--- resources/views/website/about/index.blade.php
+++ resources/views/website/about/index.blade.php
@@ -1,178 +1,229 @@
 @extends('website.layouts.master')
 @section('titel','About')
-    
+
 @section('content')
+
+<!--Start Banner-->
+
+<div class="sub-banner">
+
+  <img class="banner-img" src="images/about2.jpg" alt="">
+  <div class="detail">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="paging">
+            <h2>About Us</h2>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a>About Us</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<!--End Banner-->
+
+
+
+<!--Start Content-->
 <div class="content">
-   
-   
-    <!--Start Welcome-->
-     <div class="welcome-three">
+
+
+  <!--Start Welcome-->
+  <div class="welcome-three">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="main-title" style="padding:20px; border: solid 2px rgba(0,0,0,0.05);">
+            <h2 style="margin-bottom: 20px; "><span>About Us</span></h2>
+
+            <p style="font-family: 'Lato', sans-serif;
+                     font-weight: 400;
+                     line-height: 28px;
+                     font-size: 16px;
+                     color:#7a7a7a;padding-bottom: 125px;"> Since 2018, Noor Al-Amana Company (NA) is one of
+              well-established
+              Healthcare distribution companies in Libya.<br>
+              • Noor Al-Amana Company (NA) is a subsidiary of <a href="http://dawadigroup.com/" target="_blank"
+                style="color:black;"><b>DAWADI GROUP</b></a> Holding Company, one of the pioneer of General Trading in
+              LIBYA since 40 years.<br>
+
+
+              • Healthcare is a strategic sector Therefore , we are daily working to
+              supply the Libyan Hospitals, Pharmacies and patients with High Quality,
+              Safe and Efficient products manufactured by Lead Pharmaceutical
+              companies with whom we established a long and add valuable partnership.
+
+
+
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6" style="padding: 10px; border: solid 2px rgba(0,0,0,0.05); ">
+          <img src="images/img.jpg" alt="">
+        </div>
+      </div>
+
+
+    </div>
+    <!--End Welcome-->
+
+
+
+    <!--Start Pie Chart-->
+    <!-- <section class="piechart-section dark-back" id="pie-charts">
          <div class="container">
              
              <div class="row">
              <div class="col-md-12">
                  <div class="main-title">
-                     <h2><span>Welcome to</span>Noor Al-Amana Company </h2>
-                     <p>Since 2018, Noor Al-Amana Company (NA) is one of well-established Healthcare distribution companies in Libya.
-                         • Noor Al-Amana Company (NA) is a subsidiary of DAWADI GROUP Holding Company, one of the pioneer of General Trading in LIBYA since 40 years.
-                         • Healthcare is a strategic sector Therefore , we are daily working to supply the Libyan Hospitals, Pharmacies and patients with High Quality, Safe and Efficient products manufactured by Lead Pharmaceutical companies with whom we established a long and add valuable partnership.</p>
+                     <h2><span>Successful</span> Fatality Cases</h2>
+                     <p>If you need a doctor for to consectetuer Lorem ipsum dolor, consectetur adipiscing elit. Ut volutpat eros  adipiscing nonummy.</p>
                  </div>
              </div>
              </div>
              
-             <div class="welcome-detail">
-                 <div class="row">
-                     
-                    
-                     <div class="col-md-7">
-                         <div class="detail">
-                             
-                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                             <ul>
-                                 <li><i class="icon-checkmark"></i><span>Sed do Lorem ipsum dolor sit amet consectetur.</span></li>
-                                 <li><i class="icon-checkmark"></i><span>If you need a Heart Specialist.</span></li>
-                                 <li><i class="icon-checkmark"></i><span>Sed do Lorem ipsum dolor sit amet consectetur.</span></li>
-                                 <li><i class="icon-checkmark"></i><span>If you need a Heart Specialist.</span></li>
-                                 <li><i class="icon-checkmark"></i><span>Sed do Lorem ipsum dolor sit amet consectetur.</span></li>
-                             </ul>
-                             
-                         </div>
-                     </div>
-                     
-                     
-                      <div class="col-md-5">
-                         <img src="images/img.jpg" alt="">
-                     </div>
-                     
-                     
-                     
-                 </div>
+             <div class="text-center">
+             <span class="chart first" data-percent="95">
+                 <span class="percent">95</span>
+             <span class="year">2010</span>
+             <canvas height="181" width="181"></canvas></span>
+             
+        
+             <span class="chart" data-percent="90">
+                 <span class="percent">90</span>
+                 <span class="year">2018</span>
+             <canvas height="181" width="181"></canvas></span>
+             <span class="chart" data-percent="75">
+                 <span class="percent">75</span>
+                 <span class="year">2019</span>
+             <canvas height="181" width="181"></canvas></span>
+             <span class="chart" data-percent="95">
+                 <span class="percent">95</span>
+                 <span class="year">2020</span>
+             <canvas height="181" width="181"></canvas></span>
              </div>
              
          </div>
-     </div>
-    <!--End Welcome-->
-    
-    
-    
-    <!--Start Pie Chart-->
-    <section class="piechart-section dark-back" id="pie-charts">
-             <div class="container">
-                 
-                 <div class="row">
-                 <div class="col-md-12">
-                     <div class="main-title">
-                         <h2><span>Successful</span> Fatality Cases</h2>
-                         <p>If you need a doctor for to consectetuer Lorem ipsum dolor, consectetur adipiscing elit. Ut volutpat eros  adipiscing nonummy.</p>
-                     </div>
-                 </div>
-                 </div>
-                 
-                 <div class="text-center">
-                 <span class="chart first" data-percent="95">
-                     <span class="percent">95</span>
-                 <span class="year">2010</span>
-                 <canvas height="181" width="181"></canvas></span>
-                 
-            
-                 <span class="chart" data-percent="90">
-                     <span class="percent">90</span>
-                     <span class="year">2018</span>
-                 <canvas height="181" width="181"></canvas></span>
-                 <span class="chart" data-percent="75">
-                     <span class="percent">75</span>
-                     <span class="year">2019</span>
-                 <canvas height="181" width="181"></canvas></span>
-                 <span class="chart" data-percent="95">
-                     <span class="percent">95</span>
-                     <span class="year">2020</span>
-                 <canvas height="181" width="181"></canvas></span>
-                 </div>
-                 
-             </div>
-         </section>
+     </section> -->
     <!--End Pie Chart-->
-    
-    
+
+    <section class="piechart-section dark-back" id="pie-charts">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="main-title text-center">
+              <h2><span>Our Suppliers</span></h2>
+              <span class="" style="display: inline-block;
+             width: 61px;
+             border-bottom: solid 2px #3c763d;
+             padding-top: 10px;"></span>
+              <p class="text-center">Meet Our Suppliers</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="text-center">
+
+        </div>
+
+      </div>
+    </section>
     <!--Start Our Hospital-->
     <div class="hospital">
-    <div class="container">
-         
-         
-         
+      <div class="container">
+
+
+
         <div class="row">
           <div class="col-md-6">
-                 <div class="row">
-         <div class="col-md-12">
-             <div class="main-title">
-             <h2><span>Our</span>Vision
-             </h2>
-             <p>Lorem ipsum dolor sit amet, adipiscing elit Donec nec eros eget.</p>
-             </div>
-         </div>
-         </div>		
-             <div id="hospital">
- 
- 
-             <div class="span12">
-             <div id="services-slide" class="owl-carousel">
- 
-             <div class="item"><img src="images/service-two-img1.jpg" alt=""></div>
-             <div class="item"><img src="images/service-two-img2.jpg" alt=""></div>
-             <div class="item"><img src="images/service-two-img3.jpg" alt=""></div>
- 
-             </div>
-             </div>
- 
- 
-             </div>
-                         
-          </div>
-          
-          <div class="col-md-6">
-          <div class="why-choose-us">
-             <div class="row">
-             <div class="col-md-12">
-                 <div class="main-title">
-                 <h2><span>Why Choose</span> Medical</h2>
-                 <p>Lorem ipsum dolor sit amet, adipiscing elit Donec nec eros eget.</p>
-                 </div>
-             </div>
-             </div>
-         
-             <ul id="why-choose" class="why-choose">
-   <li class="open">
-     <div class="link"><i class="icon-first-aid-box"></i>Our Vision<i class="icon-chevron-down"></i></div>
-     <ul class="submenu" style=" display:block;">
-       <li>
-       <p>To be pioneer and leader in distribution, by building long-standing relationships with our partners and customers by providing Pharmaceutical, healthcare, and Medical Devices products of highest standards and quality in Libya and Africa.
- <br/><br/>
- 
-       </li>
-     </ul>
-   </li>
-   
-   <li>
-     <div class="link"><i class="icon-heart-beat"></i>Our Mission<i class="icon-chevron-down"></i></div>
-     <ul class="submenu">
-       <li>
-       <p> To support the Libyan Economy , mainly in Health Care Sector and start up the Pharmaceuticals & Medical Devices industry field.
- <br/><br/>
- To provide the Libyan market with all Efficient and High Quality distribution of Pharmaceutical, healthcare, and Medical supply products with a Commitment to quality health care. We value the long-term relation with our partners and Customers.</p>
-       </li>
-     </ul>
-   </li>
-  
- </ul>
-          </div>
-          </div>
-          
-         </div>
-     </div>
-     </div>
-    <!--End Our Hospital-->
-    
-      
-    </div>
+            <div class="row">
+              <div class="col-md-12" style="border: solid 2px rgba(0,0,0,0.05);padding-bottom: 50px;">
+                <div class="main-title">
+                  <h2 style="    font-size: 40px;
+         line-height: 50px;
+         font-weight: 600;"><span>Our</span>Vision
+                  </h2>
+                  <p style="font-family: 'Lato', sans-serif;
+         font-weight: 400;
+         line-height: 28px;
+         font-size: 16px;margin-bottom: 40px;
+ color: #7a7a7a; ">To be pioneer and leader in distribution, by building long-standing relationships with our partners
+                    and customers by providing Pharmaceutical, healthcare, and Medical Devices products of highest
+                    standards and quality in Libya and Africa.</p>
+                </div>
+              </div>
+            </div>
 
-@endsection
+
+          </div>
+
+          <div class="col-md-6">
+            <div class="why-choose-us">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="main-title">
+                    <h2 style="    font-size: 40px;
+             line-height: 50px;
+             font-weight: 600;"><span>Why Choose</span> Medical</h2>
+                    <p>• To support the Libyan Economy , mainly in Health Care Sector and start up the Pharmaceuticals &
+                      Medical Devices industry field. <br>
+                      • To provide the Libyan market with all Efficient and High Quality distribution of Pharmaceutical,
+                      healthcare, and Medical supply products with a Commitment to quality health care. <br>We value the
+                      long-term relation with our partners and Customers.</p>
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!--End Our Hospital-->
+
+
+  </div>
+  <!--End Content-->
+
+
+  <section class="home-two-experts-section">
+    <div class="container">
+      <div class="them-main-title text-center">
+        <h2>Our Suppliers</h2>
+        <span></span>
+        <p>Meet Our Suppliers</p>
+      </div>
+
+      <div class="container" style="padding-top: 20px; padding-bottom: 20px;">
+        <div class="row">
+          <div class="col-md-12">
+            <section class="customer-logos slider">
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+              <div class="slide"><a href="#"><img src="images/img1.jpg"></a> </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  @endsection
