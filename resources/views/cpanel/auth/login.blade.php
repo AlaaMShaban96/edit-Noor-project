15e5443bce70284e167dad85f6b6289ed9c84a1b
AlaaMShaban96 <ala96ala96@gmail.com>
2020-09-16 13:21:22 +0200

    fix login

--- resources/views/cpanel/auth/login.blade.php
+++ resources/views/cpanel/auth/login.blade.php
@@ -51,7 +51,7 @@
                           </div>
                       </div>
                       <div class="login-footer">
-                        <div class="divider">
+                        {{-- <div class="divider">
                           <div class="divider-text">OR</div>
                         </div>
                         <div class="footer-btn d-inline">
@@ -59,7 +59,7 @@
                             <a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>
                             <a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>
                             <a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>
-                        </div>
+                        </div> --}}
                       </div>
                   </div>
                   @if ($errors->any())
